56bd88fd1380b1343938176dcdc19509f8f2fd9e
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2024-03-21 17:07:28 -0300

    Cadastro finalizado

--- web/cypress/e2e/cadastro.cy.js
+++ web/cypress/e2e/cadastro.cy.js
@@ -5,18 +5,61 @@ describe('cadastro', () => {
 
     // Quando submeto o meu cadastro completo
     cy.get('[data-testid="nome"]').type('Nicolas Kumabe')
-    cy.get('[data-testid="email"]').type('pedro@4i.com.br')
+    cy.get('[data-testid="email"]').type('nicolas.k@4i.com.br')
     cy.get('[data-testid="password"]').type('estagio4i')
 
     // E clico no botão "cadastrar"
     cy.get('[data-testid="cadastrar"]').click({force: true})
 
-    // Então vejo um alerta dizendo que fui cadastrado com sucesso
+    // Entao vejo um alerta dizendo que fui cadastrado com sucesso
     cy.get('.alert').contains('Cadastro realizado com sucesso')
 
     // E sou redirecionado para a pagina de produtos
     cy.get('h1').contains('Serverest Store')
   })
 
-  
+  it('nome em branco', () => {
+    // Dado que acesso a pagina de cadastro
+    cy.visit('https://front.serverest.dev/cadastrarusuarios')
+
+    // Quando submeto o meu cadastro sem nome
+    cy.get('[data-testid="email"]').type('teste@4i.com.br')
+    cy.get('[data-testid="password"]').type('estagio4i')
+
+    // E clico no botão "cadastrar"
+    cy.get('[data-testid="cadastrar"]').click({force: true})
+
+    // Entao vejo um alerta dizendo "Nome é obrigatório"
+    cy.get('.alert').contains('Nome é obrigatório')
+  })
+
+  it('email em branco', () => {
+    // Dado que acesso a pagina de cadastro
+    cy.visit('https://front.serverest.dev/cadastrarusuarios')
+
+    // Quando submeto o meu cadastro sem nome
+    cy.get('[data-testid="nome"]').type('Nicolas Kumabe')
+    cy.get('[data-testid="password"]').type('estagio4i')
+
+    // E clico no botão "cadastrar"
+    cy.get('[data-testid="cadastrar"]').click({force: true})
+
+    // Entao vejo um alerta dizendo "Email é obrigatório"
+    cy.get('.alert').contains('Email é obrigatório')
+  })
+
+  it('senha em branco', () => {
+    // Dado que acesso a pagina de cadastro
+    cy.visit('https://front.serverest.dev/cadastrarusuarios')
+
+    // Quando submeto o meu cadastro sem nome
+    cy.get('[data-testid="nome"]').type('Nicolas Kumabe')
+    cy.get('[data-testid="email"]').type('pedro@4i.com.br')
+
+    // E clico no botão "cadastrar"
+    cy.get('[data-testid="cadastrar"]').click({force: true})
+
+    // Entao vejo um alerta dizendo "Password é obrigatório"
+    cy.get('.alert').contains('Password é obrigatório')
+  })
 })
