71a8d85882ab5d72bc85210d171d382a15704f94
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2024-03-21 17:50:54 -0300

    Senha errada

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,10 +1,17 @@
 describe('login', () => {
+  // usuario 1
+  EMAIL_1='nicolas.qa@4i.com.br'
+  SENHA_1='estagio4i'
+  //usuario 2
+  EMAIL_2='kumabe.qa.estag@4i.com.br'
+  SENHA_2='estagio4intelligence'
+
   it('criando massa de teste 1', () => {
     cy.visit('https://front.serverest.dev/cadastrarusuarios')
 
     cy.get('[data-testid="nome"]').type('Nicolas Kumabe')
-    cy.get('[data-testid="email"]').type('nicolas.qa@4i.com.br')
-    cy.get('[data-testid="password"]').type('estagio4i')
+    cy.get('[data-testid="email"]').type(`${EMAIL_1}`)
+    cy.get('[data-testid="password"]').type(`${SENHA_1}`)
 
     cy.get('[data-testid="cadastrar"]').click({force: true})
 
@@ -17,8 +24,8 @@ describe('login', () => {
     cy.visit('https://front.serverest.dev/cadastrarusuarios')
 
     cy.get('[data-testid="nome"]').type('Nicolas Kumabe')
-    cy.get('[data-testid="email"]').type('kumabe.qa.estag@4i.com.br')
-    cy.get('[data-testid="password"]').type('estagio4intelligence')
+    cy.get('[data-testid="email"]').type(`${EMAIL_2}`)
+    cy.get('[data-testid="password"]').type(`${SENHA_2}`)
 
     cy.get('[data-testid="cadastrar"]').click({force: true})
 
@@ -32,8 +39,8 @@ describe('login', () => {
     cy.visit('https://front.serverest.dev/login')
 
     // Quando submeto email e senha corretos
-    cy.get('[data-testid="email"]').type('nicolas.qa@4i.com.br')
-    cy.get('[data-testid="senha"]').type('estagio4i')
+    cy.get('[data-testid="email"]').type(`${EMAIL_1}`)
+    cy.get('[data-testid="senha"]').type(`${SENHA_1}`)
 
     // E clico no botão "entrar"
     cy.get('[data-testid="entrar"]').click({force: true})
@@ -47,40 +54,8 @@ describe('login', () => {
     cy.visit('https://front.serverest.dev/login')
 
     // Quando submeto senha sem email
-    // cy.get('[data-testid="email"]').type('nicolas.qa@4i.com.br')
-    cy.get('[data-testid="senha"]').type('estagio4i')
-
-    // E clico no botão "entrar"
-    cy.get('[data-testid="entrar"]').click({force: true})
-
-    // Entao vejo um alerta dizendo "Email é obrigatório"
-    cy.get('.alert').contains('Email é obrigatório')
-  })
-
-  
-
-  it('login com sucesso', () => {
-    // Dado que acesso a pagina de login
-    cy.visit('https://front.serverest.dev/login')
-
-    // Quando submeto email e senha corretos
-    cy.get('[data-testid="email"]').type('nicolas.qa@4i.com.br')
-    cy.get('[data-testid="senha"]').type('estagio4i')
-
-    // E clico no botão "entrar"
-    cy.get('[data-testid="entrar"]').click({force: true})
-
-    // Entao sou redirecionado para a pagina de produtos
-    cy.get('h1').contains('Serverest Store')
-  })
-
-  it('email em branco', () => {
-    // Dado que acesso a pagina de login
-    cy.visit('https://front.serverest.dev/login')
-
-    // Quando submeto senha sem email
-    // cy.get('[data-testid="email"]').type('nicolas.qa@4i.com.br')
-    cy.get('[data-testid="senha"]').type('estagio4i')
+    // cy.get('[data-testid="email"]').type(`${EMAIL_1}`)
+    cy.get('[data-testid="senha"]').type(`${SENHA_1}`)
 
     // E clico no botão "entrar"
     cy.get('[data-testid="entrar"]').click({force: true})
@@ -94,8 +69,8 @@ describe('login', () => {
     cy.visit('https://front.serverest.dev/login')
 
     // Quando submeto email sem senha
-    cy.get('[data-testid="email"]').type('nicolas.qa@4i.com.br')
-    // cy.get('[data-testid="senha"]').type('estagio4i')
+    cy.get('[data-testid="email"]').type(`${EMAIL_1}`)
+    // cy.get('[data-testid="senha"]').type(`${SENHA_1}`)
 
     // E clico no botão "entrar"
     cy.get('[data-testid="entrar"]').click({force: true})
@@ -108,9 +83,24 @@ describe('login', () => {
     // Dado que acesso a pagina de login
     cy.visit('https://front.serverest.dev/login')
 
-    // Quando submeto email sem senha
-    cy.get('[data-testid="email"]').type('kumabe.qa.estag@4i.com.br')
-    cy.get('[data-testid="senha"]').type('estagio4i')
+    // Quando submeto email errado
+    cy.get('[data-testid="email"]').type(`${EMAIL_2}`)
+    cy.get('[data-testid="senha"]').type(`${SENHA_1}`)
+
+    // E clico no botão "entrar"
+    cy.get('[data-testid="entrar"]').click({force: true})
+
+    // Entao vejo um alerta dizendo "Email e/ou senha inválidos"
+    cy.get('.alert').contains('Email e/ou senha inválidos')
+  })
+
+  it('senha errada', () => {
+    // Dado que acesso a pagina de login
+    cy.visit('https://front.serverest.dev/login')
+
+    // Quando submeto senha errada
+    cy.get('[data-testid="email"]').type(`${EMAIL_1}`)
+    cy.get('[data-testid="senha"]').type(`${SENHA_2}`)
 
     // E clico no botão "entrar"
     cy.get('[data-testid="entrar"]').click({force: true})
