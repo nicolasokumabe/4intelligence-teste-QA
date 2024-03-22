d2c93fb86f07deafaeac1aea6a35f4b9425227b3
Nicolas Kumabe <nicolas.kumabe@gmail.com>
2024-03-21 22:06:53 -0300

    Adicionando ao carrinho

--- web/cypress/e2e/4-lista_de_compras.cy.js
+++ web/cypress/e2e/4-lista_de_compras.cy.js
@@ -63,36 +63,59 @@ describe('lista de compras', () => {
     //   cy.get(':nth-child(3) > p').should('have.text', '2')
     // })
 
-    it('diminuindo a quantidade do mesmo produto', () => {
-      // Dado que estou na lista de compras
-      cy.visit('https://front.serverest.dev/login')
-      cy.get('[data-testid="email"]').type(`${email1}`)
-      cy.get('[data-testid="senha"]').type(`${senha1}`)
-      cy.get('[data-testid="entrar"]').click({force: true})
-      cy.url().should('eq', 'https://front.serverest.dev/home')
-      cy.get('h1').contains('Serverest Store')
-      cy.get('[data-testid="pesquisar"]').type(`${produto1}`)
-      cy.get('[data-testid="botaoPesquisar"]').click({force: true})
-      cy.get(':nth-child(1) > .card-body').contains(`${produto1}`)
-      cy.get(':nth-child(1) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]')
-      .click({force: true})
-      cy.get('.card').contains(`${produto1}`)
+    // it('diminuindo a quantidade do mesmo produto', () => {
+    //   // Dado que estou na lista de compras
+    //   cy.visit('https://front.serverest.dev/login')
+    //   cy.get('[data-testid="email"]').type(`${email1}`)
+    //   cy.get('[data-testid="senha"]').type(`${senha1}`)
+    //   cy.get('[data-testid="entrar"]').click({force: true})
+    //   cy.url().should('eq', 'https://front.serverest.dev/home')
+    //   cy.get('h1').contains('Serverest Store')
+    //   cy.get('[data-testid="pesquisar"]').type(`${produto1}`)
+    //   cy.get('[data-testid="botaoPesquisar"]').click({force: true})
+    //   cy.get(':nth-child(1) > .card-body').contains(`${produto1}`)
+    //   cy.get(':nth-child(1) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]')
+    //   .click({force: true})
+    //   cy.get('.card').contains(`${produto1}`)
 
-      // Quando clico 5 vezes no sinal de positivo do produto
-      for (let i = 0; i < 4; i++) {
-        cy.get('[data-testid="product-increase-quantity"]').click()
-      }
+    //   // Quando clico 5 vezes no sinal de positivo do produto
+    //   for (let i = 0; i < 4; i++) {
+    //     cy.get('[data-testid="product-increase-quantity"]').click()
+    //   }
 
-      // E clico 2 vezes no sinal de negativo do produto
-      for (let i = 0; i < 2; i++) {
-        cy.get('[data-testid="product-decrease-quantity"]').click()
-      }
+    //   // E clico 2 vezes no sinal de negativo do produto
+    //   for (let i = 0; i < 2; i++) {
+    //     cy.get('[data-testid="product-decrease-quantity"]').click()
+    //   }
 
-      // Entao a quantidade deve ser 3
-      cy.get(':nth-child(3) > p').should('have.text', '3')
-    })
+    //   // Entao a quantidade deve ser 3
+    //   cy.get(':nth-child(3) > p').should('have.text', '3')
+    // })
 
-    it('limpando a lista', () => {
+    // it('limpando a lista', () => {
+    //   // Dado que estou na lista de compras
+    //   cy.visit('https://front.serverest.dev/login')
+    //   cy.get('[data-testid="email"]').type(`${email1}`)
+    //   cy.get('[data-testid="senha"]').type(`${senha1}`)
+    //   cy.get('[data-testid="entrar"]').click({force: true})
+    //   cy.url().should('eq', 'https://front.serverest.dev/home')
+    //   cy.get('h1').contains('Serverest Store')
+    //   cy.get('[data-testid="pesquisar"]').type(`${produto1}`)
+    //   cy.get('[data-testid="botaoPesquisar"]').click()
+    //   cy.get(':nth-child(1) > .card-body').contains(`${produto1}`)
+    //   cy.get(':nth-child(1) > .card-body > div > [href="/minhaListaDeProdutos"] > [data-testid="adicionarNaLista"]')
+    //   .click({force: true})
+    //   cy.get('.card').contains(`${produto1}`)
+
+    //   // Quando clico em "Limpar Lista"
+    //   cy.get('[data-testid="limparLista"]').click({force: true})
+
+    //   // Entao devo ver a mensagem "Seu carrinho está vazio"
+    //   cy.get('[data-testid="shopping-cart-empty-message"]')
+    //   .should('have.text', 'Seu carrinho está vazio')
+    // })
+
+    it('adicionando ao carrinho', () => {
       // Dado que estou na lista de compras
       cy.visit('https://front.serverest.dev/login')
       cy.get('[data-testid="email"]').type(`${email1}`)
@@ -107,11 +130,15 @@ describe('lista de compras', () => {
       .click({force: true})
       cy.get('.card').contains(`${produto1}`)
 
-      // Quando clico em "Limpar Lista"
-      cy.get('[data-testid="limparLista"]').click({force: true})
+      // Quando clico em "Adicionar no carrinho"
+      cy.get('[data-testid="adicionar carrinho"]').click({force: true})
 
-      // Entao devo ver a mensagem "Seu carrinho está vazio"
-      cy.get('[data-testid="shopping-cart-empty-message"]')
-      .should('have.text', 'Seu carrinho está vazio')
+      // Entao devo ver a mensagem "Em construção aguarde"
+      cy.get('h1').should('have.text', 'Em construção aguarde')
+
+      // E devo ver uma imagem que não estah quebrada
+      cy.get('.jumbotron > .imagem').then(($imagem) => {
+        expect($imagem[0].naturalWidth).to.be.greaterThan(0)
+      })
     })
 })
